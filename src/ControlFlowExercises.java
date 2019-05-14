67bd34c7eb0db01e2913876da273174485477e13
Mykal Clinard <mykalclinard@gmail.com>
2019-05-14 14:00:39 -0500

    while and do-while

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+        long i = 2;
+        do {
+            System.out.println(i);
+            i= i * i;
+        }while  (i < 1000000);
+    }
 }
