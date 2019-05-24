f2f2539a04f5492e4e19881f4320e77b42f30633
Mykal Clinard <mykalclinard@gmail.com>
2019-05-24 16:18:45 -0500

    update with try and catch

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -10,7 +10,7 @@ public class InputTest {
 
 //        System.out.println(input.yesNo());
 //        System.out.println(input.getInt());
-//        System.out.println(input.getInt(5, 3));
+//        System.out.println(input.getInt(2, 20));
 //        System.out.println(input.getDouble());
 //        System.out.println(input.getDouble(3, 9));
 //        System.out.println(input.getString());
