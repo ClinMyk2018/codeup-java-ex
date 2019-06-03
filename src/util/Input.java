8ee3fcd70894557e1c3c453f01661b062f127b09
Mykal Clinard <mykalclinard@gmail.com>
2019-06-02 21:24:21 -0500

    quick save

--- src/util/Input.java
+++ src/util/Input.java
@@ -7,7 +7,7 @@ public class Input {
     private Scanner scanner;
 
     public Input(){
-        this.scanner = new Scanner(System.in).useDelimiter("\n");
+        this.scanner = new Scanner(System.in);
     }
 
     public String getString(){
